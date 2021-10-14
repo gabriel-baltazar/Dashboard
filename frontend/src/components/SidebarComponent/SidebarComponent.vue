816b54abb6a2cc3f4ef55f060f1423046a7151c2
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2021-10-14 01:20:53 -0300

    ok

--- frontend/src/components/SidebarComponent/SidebarComponent.vue
+++ frontend/src/components/SidebarComponent/SidebarComponent.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="sidebar">
-        <div class="avatar">
-            <span class="icon"><i class="fa fa-user"></i></span>
-            
-        </div>
+        <div class="top">
+            <div class="avatar">
+                <span class="icon"><i class="fa fa-user"></i></span>
+                        
+            </div>
+        
         <div class="info">
+            <p>Gabriel Baltazar</p>
+            <span>Online</span>
+        </div>
+        </div>
+
+        <div class="menu">
 
         </div>
     </div>
@@ -17,9 +25,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
- div{
-    color:white;
-    
-        
+ .sidebar{
+     color: #fff;
+       .top{
+           width: 100%;
+           display: flex;
+           justify-content: center;
+           align-items: center;
+           .avatar{
+               span{
+                   display: block;
+                   width: 50px;
+                   height: 50px;
+                   border-radius: 50%;
+                   background-color: #ccc;
+                   display: flex;
+                   justify-content: center;
+                   align-items: center;
+                   font-size: 30px;
+                   color: #000;
+
+               }
+           }
+       }
     }
 </style>
